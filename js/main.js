fe74d6c55efcd3798fd0f197d302efdba227ef52
eslam <eslamproo@yahoo.com>
2023-06-19 22:24:53 +0200

    please work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+// burger icon
 document.querySelector(".burger-icon").onclick = () =>{
     document.querySelector(".links").classList.toggle("show")
 }
+
+// bars event
+let bars = document.querySelectorAll(".bar")
+
+let activeBar = document.querySelector(".active")
+let barOne = document.querySelector(".one")
+let barTwo = document.querySelector(".two")
+let barThree = document.querySelector(".three")
+
+bars.forEach(function (ele) {
+    ele.onclick = () =>{
+        bars.forEach(function (ele) {
+            ele.classList.remove("active")
+        })
+        ele.classList.add("active")
+    }
+})
+
+// automatic moving bars
