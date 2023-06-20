3f82ded353703a90966a4a4b2e01f64e72f26805
eslam <eslamproo@yahoo.com>
2023-06-20 18:10:27 +0200

    finishing the project

--- js/main.js
+++ js/main.js
@@ -34,3 +34,39 @@ rightArrow.onclick = () => {
     imageOne.src = "images/we1.png"
     imageTwo.src = "images/we1.png"
 }
+
+// contact inputs
+
+let nameInput = document.querySelector(".input-box .name")
+let numberInput = document.querySelector(".input-box .number")
+let emailInput = document.querySelector(".input-box .email")
+let messageBox = document.querySelector(".input-box .last-input")
+let messageInput = document.querySelector(".input-box .last-input textarea")
+let btn = document.querySelector(".send-now a")
+
+btn.onclick = (e) =>{
+    if (nameInput.value === ""){
+        nameInput.style.borderBottom = "1px solid red"
+    } else {
+        nameInput.style.borderBottom = "1px solid #e6e7e8"
+    }
+    if (numberInput.value.match(/\d{11}/ig)){
+        numberInput.style.borderBottom = "1px solid #e6e7e8"
+    } else {
+        numberInput.style.borderBottom = "1px solid red"
+    }
+    if (emailInput.value.match(/\w+@\w+.(com|org|net|info)/ig)) {
+        emailInput.style.borderBottom = "1px solid #e6e7e8"
+    } else {
+        emailInput.style.borderBottom = "1px solid red"
+    }
+    if (messageInput.value === ""){
+        messageInput.style.borderBottom = "1px solid red"
+    } else {
+        messageInput.style.borderBottom = "1px solid #e6e7e8"
+    }
+    if (!numberInput.value.match(/\d{11}/ig) || !emailInput.value.match(/\D+@\D+.(com|org|net|info)/ig) || nameInput.value === "" || messageInput.value === "") {
+        e.preventDefault()
+    } 
+}
+
